ed25c6ac6fee4260d0343b399a6d436dbd3db0c6
yago <yagosonicmassaroni@gmail.com>
2024-08-06 19:30:53 -0300

    working queries

--- internal/store/pgstore/migrations/002_create_messages_table.sql
+++ internal/store/pgstore/migrations/002_create_messages_table.sql
@@ -1,13 +1,12 @@
-CREATE TABLE IF NOT EXISTS messages(
-  "id"             uuid PRIMARY KEY   NOT NULL DEFAULT gen_random_uuid(),
-  "room_id"         uuid              NOT NULL,
-  "message"         VARCHAR(255)      NOT NULL,
-  "reactions_count" BIGINT            NOT NULL DEFAULT 0,
-  "answered"        BOOLEAN           NOT NULL DEFAULT false,
+CREATE TABLE IF NOT EXISTS messages (
+    "id"                uuid            PRIMARY KEY     NOT NULL    DEFAULT gen_random_uuid(),
+    "room_id"           uuid                            NOT NULL,
+    "message"           VARCHAR(255)                    NOT NULL,
+    "reaction_count"    BIGINT                          NOT NULL    DEFAULT 0,
+    "answered"          BOOLEAN                         NOT NULL    DEFAULT false,
 
-  FOREIGN KEY(room_id) REFERENCES rooms(id)
-)
-
+    FOREIGN KEY (room_id) REFERENCES rooms(id)
+);
 
 ---- create above / drop below ----
 
