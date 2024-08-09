575e032b9e60a987d2a8a614f9524b37c3f39b40
yago <yagosonicmassaroni@gmail.com>
2024-08-09 16:55:06 -0300

    apis criadas

--- internal/api/api.go
+++ internal/api/api.go
@@ -5,7 +5,9 @@ import (
 
 	"github.com/ask-me-anything.git/internal/store/pgstore/pgstore"
 
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type apiHandler struct {
@@ -18,12 +20,53 @@ func (h apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func NewHandler(q *pgstore.Queries) http.Handler {
-	a := apiHandler {
+	a := apiHandler{
 		q: q,
 	}
 
 	r := chi.NewRouter()
+	r.Use(middleware.RequestID, middleware.Recoverer, middleware.Logger)
+
+	
+
+	r.Route("/api", func(r chi.Router) {
+		r.Route("rooms", func(r chi.Router) {
+			r.Post("/", a.handleCreateRoom)
+			r.Get("/", a.handleGetRooms)
+
+			r.Route("/{room_id}/messages", func(r chi.Router) {
+				r.Post("/", a.handleCreateRoomMessage)
+				r.Get("/", a.handleGetRoomMessages)
+
+				r.Route("/${message_id}", func(chi.Router) {
+					r.Get("/", a.handleGetRoomMessage)
+					r.Patch("/react", a.handleReactToMessage)
+					r.Delete("/react", a.handleRemoveReactFromMessage)
+					r.Patch("/react", a.handleMarkMessageAsAnswered)
+
+				})
+			})
+		})
+	})
 
 	a.r = r
 	return a
 }
+
+func (h apiHandler) handleCreateRoom(w http.ResponseWriter, r *http.Request) {}
+
+func (h apiHandler) handleGetRooms(w http.ResponseWriter, r *http.Request) {}
+
+func (h apiHandler) handleCreateRoomMessage(w http.ResponseWriter, r *http.Request) {}
+
+func (h apiHandler) handleGetRoomMessages(w http.ResponseWriter, r *http.Request) {}
+
+func (h apiHandler) handleGetRoomMessage(w http.ResponseWriter, r *http.Request) {}
+
+func (h apiHandler) handleReactToMessage(w http.ResponseWriter, r *http.Request) {}
+
+func (h apiHandler) handleRemoveReactFromMessage(w http.ResponseWriter, r *http.Request) {}
+
+func (h apiHandler) handleMarkMessageAsAnswered(w http.ResponseWriter, r *http.Request) {}
+
+
